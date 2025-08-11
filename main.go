590dbde7244e6e9d4a04c2dcea5af3f0eb408a18
Tom Webber <t.c.w.webber@gmail.com>
2025-08-11 21:03:30 +0200

    feat(read): list all running processes

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
-package gotop
+package main
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"os"
+)
+
+func main() {
+	files, err := os.ReadDir("/proc")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			continue
+		}
+		pid := file.Name()
+		if _, err := strconv.Atoi(pid); err != nil {
+			continue
+		}
+		cmdlinePath := filepath.Join("/proc", pid, "cmdline")
+		data, err := os.ReadFile(cmdlinePath)
+		if err != nil || len(data) == 0 {
+			continue
+		}
+		cmdline := strings.ReplaceAll(string(data), "\x00", " ")
+		fmt.Println(cmdline)
+
+	}
+}
